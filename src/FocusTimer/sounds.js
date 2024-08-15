4d3e4da6580f74f2ce471e14f9a5dd117595f142
Arthur Erpen <77343288+artherp@users.noreply.github.com>
2024-08-15 14:43:54 -0300

    audios e inicio do main

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,12 @@
+export const buttonPress = new Audio('/assets/ButtonPress.wav')
+export const timer = new Audio('/assets/kichen-timer.mp3')
+
+export const forest = new Audio('/assets/Floresta.wav')
+export const rain = new Audio('/assets/Chuva.wav')
+export const coffee = new Audio('/assets/Cafeteria.wav')
+export const fireplace = new Audio('/assets/Lareira.wav')
+
+forest.loop = true;
+rain.loop = true;
+coffee.loop = true;
+fireplace.loop = true;

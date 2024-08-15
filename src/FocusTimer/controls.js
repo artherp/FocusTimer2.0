96f9153c69f3678626e90d2483c7e12ffb007172
Arthur Erpen <77343288+artherp@users.noreply.github.com>
2024-08-15 14:58:07 -0300

    const buttons definidas

---
+++ src/FocusTimer/controls.js
@@ -0,0 +1,12 @@
+export const timerControls = document.querySelector('.controls-main')
+export const controlsMusics = document.querySelector('.controls-musics')
+export const addBtn = document.getElementById('add')
+export const subBtn = document.getElementById('subtract')
+
+export const forestBtn = document.querySelector("#forest-sound-btn")
+export const rainBtn = document.querySelector("#rain-sound-btn");
+export const coffeeBtn = document.querySelector("#house-sound-btn");
+export const fireplaceBtn = document.querySelector("#fire-sound-btn");
+
+export const minutes = document.querySelector("#minutes");
+export const seconds = document.querySelector("#seconds");
